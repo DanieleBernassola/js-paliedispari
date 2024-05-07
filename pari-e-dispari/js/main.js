5e6db127023b4b01c5f9b2e298e92c37443e86ea
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-07 17:56:54 +0200

    [UPD] Added comments

--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -1,16 +1,21 @@
 'use strict';
 
-const sceltaUtente = prompt('Scegli pari o dispari?');
+// Dati di input 
+let sceltaUtente = prompt('Scegli pari o dispari?').toLowerCase();
+console.log('Hai scelto:', sceltaUtente);
 const numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
-console.log(numeroUtente);
+console.log('Questo è il numero che hai inserito.', numeroUtente);
 
+// Funzione genera numero intero
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+// Somma numero inserito con numero generato da 1 a 5
 let somma = numeroUtente + getRndInteger(1,5);
-console.log(somma);
+console.log('Questo è il risultato dei vostri numeri sommati:', somma);
 
+// Funzione che verifica se resto della somma è pari o dispari
 function pariDispari(somma){
     if (somma % 2 === 0){
         return "pari";
@@ -19,6 +24,7 @@ function pariDispari(somma){
     }
 }
 
+// Condizione finale
 if (pariDispari(somma) === sceltaUtente) {
     console.log('Hai vinto contro il computer');
 } else {
